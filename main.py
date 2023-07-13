ae7bebaba7c2f63068edf4f620042c71922d23d7
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2023-07-13 22:23:40 +0100

    creation of subfolder with functions

--- main.py
+++ main.py
@@ -1,49 +1,81 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright, sync_playwright
 
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(
-    geolocation={"longitude": 51.136502, "latitude": 22.413220},
-    permissions=["geolocation"]
-    )
-    page = context.new_page()
-    page.goto("https://ads.tiktok.com/business/creativecenter/inspiration/popular/hashtag/pc/en")
-    
-    #change region
-    page.get_by_role("button", name="Morocco").click()
-    print("[1] Region changed")
+class Scraper:
+    def __init__(self, playwright: Playwright):
+        self.playwright = playwright
+        self.browser = None
+        self.context = None
+        self.page = None
 
-    #set to uk
-    page.get_by_placeholder("Start typing or select from the list").click()
-    page.get_by_placeholder("Start typing or select from the list").fill("united kin")
-    page.get_by_test_id("cc_rimless_select_undefined_item_68").click()
-    print("[2] Set to UK")
+    def setup(self):
+        self.browser = self.playwright.chromium.launch(headless=False)
+        self.context = self.browser.new_context(
+            geolocation={"longitude": -0.293679, "latitude": 51.453212},
+            permissions=["geolocation"],
+            locale='en-GB',
+            timezone_id='Europe/London',
+        )
+        self.page = self.context.new_page()
 
-    #change hashtags
-    page.locator("#hashtagIndustrySelect").get_by_role("img").click()
-    page.get_by_test_id("cc_single_select_undefined_item_8").get_by_text("Health").click()
-    print("[3] Set to #health")
+    def teardown(self):
+        self.context.close()
+        self.browser.close()
 
-    #viewing more hashtags
-    for i in range(3):
-        page.get_by_test_id("cc_contentArea_viewmore_btn").get_by_text("View More").click()
-    print("[4] pressing on view more button to get more hashtags")
+    def tiktokHashtags(self,timeout=0):
+        self.setup()
+        self.page.goto("https://ads.tiktok.com/business/creativecenter/inspiration/popular/hashtag/pc/en")
 
-    #getting hashtags
-    health_hashtags = page.query_selector_all('.CardPc_titleText__RYOWo')
-    print("[5] Got hashtags")
-    for hashtag in health_hashtags:
-        print(hashtag.inner_text())
-    
+        # [1] Change region
+        self.page.click('span[data-testid="cc_rimless_select_undefined"]')
+        print("[1] Region changed")
 
-    page.wait_for_timeout(10000)
-    context.close()
-    browser.close()
+        # [2] Set to UK
+        self.page.click('[placeholder="Start typing or select from the list"]')
+        self.page.fill('[placeholder="Start typing or select from the list"]', "united kin")
+        self.page.get_by_test_id("cc_rimless_select_undefined_item_68").click()
+        print("[2] Set to UK")
+
+        # [3] Change hashtags
+        self.page.locator("#hashtagIndustrySelect").get_by_role("img").click()
+        self.page.get_by_test_id("cc_single_select_undefined_item_8").get_by_text("Health").click()
+        print("[3] Set to #health")
+
+        # [4] View more hashtags
+        for _ in range(3):
+            self.page.get_by_test_id("cc_contentArea_viewmore_btn").get_by_text("View More").click()
+        print("[4] Pressed view more button to get more hashtags")
+
+        # [5] Getting hashtags
+        health_hashtags = self.page.query_selector_all('.CardPc_titleText__RYOWo')
+        print("[5] Got hashtags")
+        for hashtag in health_hashtags:
+            print(hashtag.inner_text())
+        self.page.wait_for_timeout(timeout)
+        self.teardown()
+
+    def googleMaps(self,timeout=10000):
+        #this function allows to check geolocation settings
+        #you should click on button "Your location", otherwise it'll be your real location
+        self.setup()
+        self.page.goto("https://maps.google.com")
+        self.page.wait_for_timeout(timeout)
+        self.teardown()
+
+    def regionTiktok(self,timeout=0):
+        self.setup()
+        self.page.goto("https://ads.tiktok.com/business/creativecenter/inspiration/popular/hashtag/pc/en")
+        self.page.wait_for_timeout(timeout)
+        self.teardown()
+
+    def browserOpen(self):
+        self.setup()
+        self.page.goto("https://google.com")
+        while True:
+            pass
+
 
 
 with sync_playwright() as playwright:
-    run(playwright)
-
-#CardPc_titleText__RYOWo
-# playwright codegen demo.playwright.dev/todomvc  
+    scraper = Scraper(playwright)
+    scraper.tiktokHashtags()
