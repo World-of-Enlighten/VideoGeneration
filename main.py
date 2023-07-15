96a256e446d6fe52a0bf9c789f3f420d5b1c5aaf
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2023-07-15 11:27:39 +0100

    1st route

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from src.scraper import Scraper
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
-
+scraper = Scraper()
 @app.route('/')
 def index():
     return "API is alive, check the <a href='https://github.com/World-of-Enlighten/TikTokTrendsAPI/blob/main/README.md'>readme</a> for usage instructions."
+
+
+@app.route('/api/tiktok/hashtags/',methods=['POST'])
+def getTiktokHashtags():
+    dat = request.get_json()
+    if 'numHashtags' in dat:
+        numHashtags=dat['numHashtags']
+    else:
+        numHashtags=9
+    if 'tag' in dat:
+        tag=dat['tag']
+    else:
+        tag='Health'
+    return scraper.tiktokHashtags()
 
 if __name__ == '__main__':
     app.run(debug=True)
